b3bb651e7916c1869ca72e3c205078304e3a92af
Damian Łączyński <damian.laczynski.edu@gmail.com>
2024-02-18 17:50:30 +0100

    Add new custom exceptions

---
+++ StudyBook.API/Exceptions/ForbiddenException.cs
@@ -0,0 +1,20 @@
+﻿using System.Runtime.Serialization;
+
+namespace StudyBookAPI.Exceptions
+{
+    [Serializable]
+    internal class ForbiddenException : Exception
+    {
+        public ForbiddenException()
+        {
+        }
+
+        public ForbiddenException(string message) : base(message)
+        {
+        }
+
+        public ForbiddenException(string message, Exception innerException) : base(message, innerException)
+        {
+        }
+    }
+}
