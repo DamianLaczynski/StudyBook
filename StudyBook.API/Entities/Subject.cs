837e1940b1f3eb064cfbc758433672e0befafb2a
Damian Łączyński <damian.laczynski.edu@gmail.com>
2024-03-03 01:02:00 +0100

    Update subject entiti

--- StudyBook.API/Entities/Subject.cs
+++ StudyBook.API/Entities/Subject.cs
@@ -7,7 +7,7 @@ namespace StudyBook.API.Entities
         public int Id { get; set; }
         public string Name { get; set; }
         public string? Description { get; set; }
-        public List<FlashcardSet> FlashcardSets { get; set; }
+        public List<FlashcardSet> FlashcardSets { get; set; } = new List<FlashcardSet>();
         public List<Test> Tests { get; set; } = new List<Test>();
     }
 }
---
+++ StudyBook.API/Migrations/20240219185739_addDefaultValueForFlashcardSetInSubject.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace StudyBookAPI.Migrations
+{
+    /// <inheritdoc />
+    public partial class addDefaultValueForFlashcardSetInSubject : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+
+        }
+    }
+}
